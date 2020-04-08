a89e7b847c665f6bf5f594659a96028deedeceb0
emil2 <emil@netcompany.com>
2020-04-08 14:09:32 +0200

    Zajęcia 2 - zadanie 2

--- ksiazki.szczegoly.php
+++ ksiazki.szczegoly.php
@@ -21,7 +21,16 @@ $dane = $ksiazki->pobierz($id)
     <p>
         <a href="ksiazki.lista.php"><i class="fas fa-chevron-left"></i> Powrót</a>
     </p>
-
-    <p>szczegóły książki......</p>
+    <?php if (!empty($dane['zdjecie'])): ?>
+        <img style="width: 30%" src="zdjecia/<?= $dane['zdjecie'] ?>" alt="<?= $dane['tytul'] ?>" class="img-thumbnail"/></br>
+    <?php else: ?>
+        brak zdjęcia</br>
+    <?php endif; ?>
+    </br>
+    <p> ISBN : <?= $dane['isbn'] ?></p>
+    <p> Liczba stron : <?= $dane['liczba_stron'] ?></p>
+    <p> Cena : <strong><?= $dane['cena'] ?>zł</strong></p>
+    <p> Opis : <?= $dane['opis'] ?></p>
+    
 
 <?php include 'footer.php'; ?>
